044f2b423b0c049120da419cff0a399b07c70a03
Vitor Ferreira <victor-bash2@hotmail.com>
2020-11-24 15:28:49 -0300

    Alteração da tela de pesquisa

--- resources/views/inicio/pesquisa.blade.php
+++ resources/views/inicio/pesquisa.blade.php
@@ -1,3 +1,7 @@
 @extends('template.layout1')
 @section('conteudo')
+<form method="get" action="{{route('callApi')}}">
+<div class="search-box">
+    <input name="nomeHeroi" type="text" placeholder="Digite o nome do herói"/><span></span>
+</div>
 @endsection
